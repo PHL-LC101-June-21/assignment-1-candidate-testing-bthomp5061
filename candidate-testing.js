cda013a22aee5c230a57d2dd7e0472ec05ea170e
bthomp5061 <85450715+bthomp5061@users.noreply.github.com>
2021-06-13 19:53:55 +0000

    CanidateAnser

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,10 +1,16 @@
 const input = require('readline-sync');
  
 // TODO 2: modify your quiz app to ask 5 questions //
- 
+
 // TODO 1.1a: Define candidateName //
-let candidateName;
+ let candidateName = input.question("What is your name? ");
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
++ let candidateAnswer;
+
+
 let question;
 let correctAnswer;
 let candidateAnswer;
@@ -15,31 +21,35 @@ let candidateAnswers;
  
 function askForName() {
  // TODO 1.1b: Ask for candidate's name //
- 
+ let candidateName = input.question("What is your name? ");
 }
- 
-function askQuestion() {
+
+
  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- 
- 
-}
- 
+ function askQuestion(){
+ candidateAnswer = input.question(question);
+ }
+
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
 function gradeQuiz(candidateAnswers) {
- 
- // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
- 
- 
- let grade;
- 
- return grade;
+if (candidateAnswer === correctAnswer){
+ let grade; = passed
++ return grade;
+}else{
+  let grade = "Failed";
++ return grade:
 }
- 
-function runProgram() {
+}
++ function runProgram() {
  askForName();
  // TODO 1.1c: Ask for candidate's name //
-  askQuestion();
- gradeQuiz(this.candidateAnswers);
+console.log("Hello, " + candidateName);
+askQuestion();
+gradeQuiz(this.candidateAnswers);
++ grade = gradeQuiz(this.candidateAnswers);
++ console.log("You " + grade);
 }
+
  
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected 
--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 const input = require('readline-sync');
+ 
+// TODO 2: modify your quiz app to ask 5 questions //
 
-let questions = ["Candidate Name:  ", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?","Given the array [8, Orbit, Trajectory, 45]what entry is at index 2?","Who was the first American woman in space? ","What is the minimum crew size for the International Space Station(ISS)?"];
-let answers = ["Can Twin","True","40","tajectory","sally ride","3"];
-let response = [];
-let correctAnswers = 0;
-for (let i = 0; i < questions.length; i++) {
-     response[i] = input.question (questions[i]
-     );
-  if (response[i].toUpperCase() == answers[i].toUpperCase()){
-    correctAnswers++; 
-  }
+// TODO 1.1a: Define candidateName //
+ let candidateName = input.question("What is your name? ");
+
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let quizquestion = ("Who was the first American woman in space?");
+let correctAnswer = ("Sally Ride");
++ let canidateanswer;
+let candidateAnswer ="";
+let question;
+let correctAnswer;
+let questions;
+let correctAnswers;
+let candidateAnswers;
+ 
+ 
+function askForName() {
+ // TODO 1.1b: Ask for candidate's name //
+ let candidateName = input.question("What is your name? ");
 }
-let score= correctAnswers / questions.length;
-console.log("CAN TWIN");
-console.log("Correct Answers-   " + score);
-console.log(score * 100 + "% on test");
-if ([score * 100] > [8 * 100])
-console.log("PASS!");
-else if ([score * 100] < [8 * 100])
-console.log("FAIL!");
 
 
+ // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+ + let grade:
+ function askQuestion(){
+ candidateAnswer = input.question(question);
+ }
+
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+function gradeQuiz(candidateAnswers) {
+if (candidateAnswer === correctAnswer){
+ let grade; = passed
++ return grade;
+}else{
+  let grade = "Failed";
++ return grade:
+}
+}
++ function runProgram() {
+ askForName();
+ // TODO 1.1c: Ask for candidate's name //
+console.log("Hello, " + candidateName);
+askQuestion();
+gradeQuiz(this.candidateAnswers);
++ grade = gradeQuiz(this.candidateAnswers);
++ console.log("You " + grade);
+}
+
+ 
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected 
+
+module.exports = {
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
+};
