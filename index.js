a013adc9ea26d170e04c5497a3f2a736bbcadcab
bthomp5061 <85450715+bthomp5061@users.noreply.github.com>
2021-06-18 01:29:43 +0000

    Assignment 2

--- index.js
+++ index.js
@@ -1,69 +1,21 @@
-const quiz = require('./candidate-testing');
-
 const input = require('readline-sync');
- 
-// TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName //
-let candidateName = "";
-
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer /
-
-let question = " Who was the first American woman in space? ";
-let correctAnswer = " Sally Ride ";
-let candidateAnswer = "" ;
-let questions
-let correctAnswers 
-let candidateAnswers 
- 
-quiz.runProgram();
-function askForName() {
- // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question("What is your name?");
+let questions = ["Candidate Name:  ", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?","Given the array [8, Orbit, Trajectory, 45]what entry is at index 2?","Who was the first American woman in space? ","What is the minimum crew size for the International Space Station(ISS)?"];
+let answers = ["Can Twin","True","40","tajectory","sally ride","3"];
+let response = [];
+let correctAnswers = 0;
+for (let i = 0; i < questions.length; i++) {
+     response[i] = input.question (questions[i]
+     );
+  if (response[i].toUpperCase() == answers[i].toUpperCase()){
+    correctAnswers++; 
+  }
 }
-
-function askQuestion(){
- // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- 
- candidateAnswer = input.question; 
- }
-
-function gradeQuiz(candidateAnswers) {
-// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-
- if(candidateAnswer === "Sally Ride") {
-    console.log("Correct Answer"); 
-  }
-   else {
-    console.log("Incorrect Answer");
-  }
-   let grade; 
-
-   return grade;
-  }
-  function runProgram() {
- askForName();
-
- // TODO 1.1c: Ask for candidate's name //
-console.log(" Hello, " +  candidateName + "!");
-
- askQuestion();
- gradeQuiz(this.candidateAnswers);
-
-}
-
- 
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected 
-
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+let score= correctAnswers / questions.length;
+console.log("CAN TWIN");
+console.log("Correct Answers-   " + score);
+console.log(score * 100 + "% on test");
+if ([score * 100] > [8 * 100])
+console.log("PASS!");
+else if ([score * 100] < [8 * 100])
+console.log("FAIL!");
