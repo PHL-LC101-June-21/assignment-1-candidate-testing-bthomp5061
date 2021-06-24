fdfaecd94ada949568f115d7e2c86742953dc82b
bthomp5061 <85450715+bthomp5061@users.noreply.github.com>
2021-06-24 19:00:14 +0000

    assignment 3 final1

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
-const input = require('readline-sync');
 const quiz = require('./candidate-testing');
+quiz.runProgram();
 
 
-let questions = ["Candidate Name:  ", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?","Given the array [8, Orbit, Trajectory, 45]what entry is at index 2?","Who was the first American woman in space? ","What is the minimum crew size for the International Space Station(ISS)?"];
-let answers = ["Can Twin","True","40","tajectory","sally ride","3"];
-let response = [];
-let correctAnswers = 2;
+// TODO 2: modify your quiz app to ask 5 questions //
+// TODO 1.1a: Define candidateName // 
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+
+
+let candidateName = "";
+let question = "Who was the first American woman in space? ";
+let questions = [  "Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswer = "Sally Ride";
+let correctAnswers = ["Sally Ride",  "true",  "40",  "Trajectory",  "3"];
+let candidateAnswer = "";
+let candidateAnswers = [];
+let consoleTest = "";
+
+
+  // TODO 1.1b: Ask for candidate's name //
+
+function askForName() {
+	candidateName = input.question("Candidate Name: ")
+	return console.log(`Hello, ${candidateName}.`);
+}
+
+
+// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer
 
 function askQuestion() {
 	for (let i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = input.question(`${questions[i]}
-Your Answer: `);
-         console.log(`Correct Answer: ${correctAnswers[i]}`);
+		candidateAnswers[i] = input.question(`${questions[i]} Your Answer:`);
+		console.log(`Correct Answer: ${candidateAnswer[i]}`);
 	}
 	return;
 }
 
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
 
 
+function gradeQuiz(candidateAnswers) {
+	let grade = 0;
+	for (let i = 0; i < candidateAnswers.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      grade++;
+		}
+  }
 
+candidateAnswer = grade;
+grade/correctAnswers.length*100;
+  console.log(`>>> Overall Grade: ${grade}% (${(candidateAnswer) * 5} of 5 responses correct) <<<`)
+  if (grade >= 80) {
+    console.log(`>>> Status: PASSED <<<`)
+  } else {
+    console.log(`>>> Status: FAILED <<<`)
+  }
+  return grade;
+	}
+// TODO 1.1c: Ask for candidate's name 
+function runProgram() {
+  askForName();
+  askQuestion();
+  gradeQuiz(candidateAnswers)
+}
 
-let score= correctAnswers / questions.length;
-console.log("CAN TWIN");
-console.log("Correct Answers-   " + score);
-console.log(score * 100 + "% on test");
-if ([score * 100] > [8 * 100])
-console.log("PASS!");
-else if ([score * 100] < [8 * 100])
-console.log("FAIL!");
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
+module.exports = {
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
+};
